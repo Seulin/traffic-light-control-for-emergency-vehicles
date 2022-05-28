03adc6deb503deee09afe6c21f509bc63dd0e633
yooshin0303 <yooshin0303@dgist.ac.kr>
2022-05-28 16:50:57 +0900

    stable_baselines to stable_lines3 of dqn and map number, 27 to 4 of ql

--- dqn_main.py
+++ dqn_main.py
@@ -11,7 +11,7 @@ import numpy as np
 from sumo_rl import SumoEnvironment
 import traci
 
-from stable_baselines import DQN
+from stable_baselines3.dqn.dqn import DQN
 
 from map_file import get_map, out_folder
 
@@ -26,7 +26,7 @@ env = SumoEnvironment(net_file=net_file,
                         single_agent=True,
                         route_file=rou_file, 
                         out_csv_name= out_folder(tag) + 'dqn', #'outputs/big-intersection/dqn',
-                        use_gui=False,
+                        use_gui=True,
                         num_seconds=5400,
                         yellow_time=4,
                         min_green=5,
